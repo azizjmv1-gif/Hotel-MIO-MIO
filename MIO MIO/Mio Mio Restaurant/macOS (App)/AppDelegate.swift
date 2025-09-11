f08d4d03e5ec0a5959ca52fd9483d2de93f63ec7
azizjmv1-gif <aziz.jmv1@gmail.com>
2025-09-11 11:59:14 +0200

    Add project files and structure

---
+++ MIO MIO/Mio Mio Restaurant/macOS (App)/AppDelegate.swift
@@ -0,0 +1,21 @@
+//
+//  AppDelegate.swift
+//  macOS (App)
+//
+//  Created by 𝐀𝐙𝐈𝐙 𝐉𝐔𝐌𝐀𝐁𝐄𝐈 on 10.09.25.
+//
+
+import Cocoa
+
+@main
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    func applicationDidFinishLaunching(_ notification: Notification) {
+        // Override point for customization after application launch.
+    }
+
+    func applicationShouldTerminateAfterLastWindowClosed(_ sender: NSApplication) -> Bool {
+        return true
+    }
+
+}
